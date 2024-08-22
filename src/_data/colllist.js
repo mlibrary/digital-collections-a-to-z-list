0054dc891fd2afc64409ad1a58e63afbf2332b12
Roger Espinosa <roger@umich.edu>
2024-08-22 13:04:44 -0400

    start prep for building only when necessary

--- src/_data/colllist.js
+++ src/_data/colllist.js
@@ -1,11 +1,20 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
+const fs = require('node:fs');
+const process = require('node:process');
 
 module.exports = async function () {
+  let jsonFilename = process.env.COLLLIST_DATA;
   let url = 'https://quod.lib.umich.edu/cgi/c/collsize/colllist?sponsor=DCC';
-  let data = await EleventyFetch(url, {
-    duration: '1d',
-    type: 'json',
-  });
+  let data;
+  if ( jsonFilename && fs.existsSync(jsonFilename) ) {
+    console.log("-- colllist: using local file");
+    data = require(jsonFilename); // JSONfs.readFileSync(jsonFilename);
+  } else {
+    data = await EleventyFetch(url, {
+      duration: '1d',
+      type: 'json',
+    })
+  }
   data.items.sort((a, b) => 
     a.label.localeCompare(
       b.label, 
